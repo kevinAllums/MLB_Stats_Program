ee36d23ce2a0eabaed76b8dea6be2865d4e571e7
Kevin Allums <kevin.allums@protonmail.com>
2017-07-11 16:51:12 -0500

    Add Team Class

---
+++ MLB_Stats/MLB_Stats/Team.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MLB_Stats
+{
+    class Team
+    {
+        private string yearID;
+        private string name;
+        private string teamID;
+
+        private DataTable teamBasicInfo;
+        private DataTable teamBattingStats;
+        private DataTable teamPitchingStats;
+        private DataTable teamBattingStatsPost;
+        private DataTable teamPitchingStatsPost;
+
+        private StatsDatabaseAccess statsDatabaseAccess = new StatsDatabaseAccess();
+
+        public Team(string yearID, string name)
+        {
+            this.yearID = yearID;
+            this.name = name;
+            this.teamID = statsDatabaseAccess.getTeamID(this.yearID, this.name);
+            this.teamBasicInfo = statsDatabaseAccess.getTeamBasicInfo(this.yearID, this.name);
+            this.teamBattingStats = statsDatabaseAccess.getTeamBattingStats(this.yearID, this.teamID);
+            this.teamPitchingStats = statsDatabaseAccess.getTeamPitchingStats(this.yearID, this.teamID);
+            this.teamBattingStatsPost = statsDatabaseAccess.getTeamBattingStatsPost(this.yearID, this.teamID);
+            this.teamPitchingStatsPost = statsDatabaseAccess.getTeamPitchingStatsPost(this.yearID, this.teamID);
+        }
+
+        public string YearID
+        {
+            get
+            {
+                return yearID;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public DataTable TeamBasicInfo
+        {
+            get
+            {
+                return teamBasicInfo;
+            }
+        }
+
+        public DataTable TeamBattingStats
+        {
+            get
+            {
+                return teamBattingStats;
+            }
+        }
+
+        public DataTable TeamPitchingStats
+        {
+            get
+            {
+                return teamPitchingStats;
+            }
+        }
+
+        public DataTable TeamBattingStatsPost
+        {
+            get
+            {
+                return teamBattingStatsPost;
+            }
+        }
+
+        public DataTable TeamPitchingStatsPost
+        {
+            get
+            {
+                return teamPitchingStatsPost;
+            }
+        }
+    }
+}
