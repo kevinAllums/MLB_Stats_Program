94abb645c4f01b2465bf0517a209cdace19c38b7
Kevin Allums <kevin.allums@protonmail.com>
2017-07-11 16:24:35 -0500

    Create the MainWindow Add Images

---
+++ MLB_Stats/MLB_Stats/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MLB_Stats
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.Height = SystemParameters.MaximizedPrimaryScreenHeight / 2;
+            this.Width = SystemParameters.PrimaryScreenWidth / 2;
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
+            InitializeComponent();
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Search_Click(object sender, RoutedEventArgs e)
+        {
+            //Search search = new Search();
+            //search.ShowDialog();
+        }
+    }
+}
